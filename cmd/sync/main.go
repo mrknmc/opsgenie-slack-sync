0fb4e8e22cf8ed7ab892f5412924e74da5f609ab
Mark Nemec <mrknmc@me.com>
2021-10-27 18:54:56 +0200

    First commit

---
+++ cmd/sync/main.go
@@ -0,0 +1,82 @@
+package sync
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/opsgenie/opsgenie-go-sdk-v2/client"
+	"github.com/opsgenie/opsgenie-go-sdk-v2/schedule"
+	log "github.com/sirupsen/logrus"
+	"github.com/slack-go/slack"
+)
+
+const scheduleName string = "Engineering_schedule"
+const userGroup string = "eng-support"
+
+type Syncer struct {
+	Slack    *slack.Client
+	OpsGenie *schedule.Client
+}
+
+func (s *Syncer) WhoIsOnCall() ([]string, error) {
+	flat := false
+	scheduleResult, err := s.OpsGenie.GetOnCalls(context.TODO(), &schedule.GetOnCallsRequest{
+		Flat:                   &flat,
+		ScheduleIdentifierType: schedule.Name,
+		ScheduleIdentifier:     scheduleName,
+	})
+	if err != nil {
+		return nil, err
+	}
+	participants := scheduleResult.OnCallParticipants
+	users := make([]string, len(participants))
+	for i, p := range participants {
+		users[i] = p.Name
+	}
+	return users, nil
+}
+
+func (s *Syncer) UpdateUserGroup(groupName string, ids []string) error {
+	_, err := s.Slack.UpdateUserGroupMembers(userGroup, strings.Join(ids, ","))
+	return err
+}
+
+func (s *Syncer) GetSlackId(email string) (string, error) {
+	user, err := s.Slack.GetUserByEmail(email)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("<@%s>", user.ID), nil
+}
+
+func main() {
+	log.SetLevel(log.InfoLevel)
+	var scheduleClient, _ = schedule.NewClient(&client.Config{
+		ApiKey: os.Getenv("OPSGENIE_API_KEY"),
+	})
+
+	var syncer = Syncer{
+		Slack:    slack.New(os.Getenv("SLACK_BOT_TOKEN")),
+		OpsGenie: scheduleClient,
+	}
+
+	onCallUsers, err := syncer.WhoIsOnCall()
+	if err != nil {
+		panic(err)
+	}
+
+	// convert emails to slack ids
+	slackIds := make([]string, len(onCallUsers))
+	for i, email := range onCallUsers {
+		slackId, err := syncer.GetSlackId(email)
+		if err != nil {
+			slackIds[i] = slackId
+		} else {
+			log.Error("Could not convert email %s to slack id", email)
+		}
+	}
+
+	syncer.UpdateUserGroup(userGroup, slackIds)
+}
---
+++ go.mod
@@ -0,0 +1,34 @@
+module github.com/mrknmc/opsgenie-slack-sync
+
+go 1.17
+
+require (
+	github.com/spf13/viper v1.9.0
+	github.com/sirupsen/logrus v1.4.2
+	github.com/opsgenie/opsgenie-go-sdk-v2 v1.2.8
+	github.com/slack-go/slack v0.9.5
+	github.com/stretchr/testify v1.7.0
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.5.1 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/spf13/afero v1.6.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+	golang.org/x/text v0.3.6 // indirect
+	gopkg.in/ini.v1 v1.63.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.1.1 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
